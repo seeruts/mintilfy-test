e86aef546215129ce31345562f47c413d6b70e99
seeruts <131783923+seeruts@users.noreply.github.com>
2024-09-29 22:01:16 -0700

    Update test.js

--- test.js
+++ test.js
@@ -2,4 +2,32 @@ function showHelloWorldPopup() {
   alert("Hello, World!");
 }
 
-window.onload = showHelloWorldPopup;
+
+
+let paramString = urlString.split('?')[1];
+let queryString = new URLSearchParams(paramString);
+for (let pair of queryString.entries()) {
+   console.log("Key is: " + pair[0]);
+   console.log("Value is: " + pair[1]);
+  if (pair[0] = 'userinfo') {
+    test=true
+  }
+}
+
+if (test) {
+type UserInfo = {
+  expiresAt?: number;
+  groups?: 'admin';
+  content?: Record<string, any>;
+  apiPlaygroundInputs?: {
+    header?: Record<string, any>;
+    query?: Record<string, any>;
+    cookie?: Record<string, any>;
+    server?: Record<string, string>;
+  }
+}
+}
+
+if (test) {
+  window.onload = showHelloWorldPopup;
+}
